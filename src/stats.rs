eb5093efe6fd8ceb53fe873a082359620ce1639e
Canop <cano.petrole@gmail.com>
2022-03-01 18:58:40 +0100

    consider stats with 0 blocks as invalid

--- src/stats.rs
+++ src/stats.rs
@@ -31,22 +31,36 @@ impl Stats {
             let code = libc::statvfs(c_mount_point.as_ptr(), statvfs.as_mut_ptr());
             match code {
                 0 => {
-                    // good
                     let statvfs = statvfs.assume_init();
+
+                    // blocks info
+                    let bsize = statvfs.f_bsize as u64;
+                    let blocks = statvfs.f_blocks as u64;
+                    let bfree = statvfs.f_bfree as u64;
+                    let bavail = statvfs.f_bavail as u64;
+                    if bsize == 0 || blocks == 0 || bfree > blocks || bavail > blocks {
+                        // unconsistent or void data
+                        return None;
+                    }
+
+                    // inodes info, will be checked in Inodes::new
                     let files = statvfs.f_files as u64;
                     let ffree = statvfs.f_ffree as u64;
                     let favail = statvfs.f_favail as u64;
+                    let inodes = Inodes::new(files, ffree, favail);
+
                     Some(Stats {
-                        bsize: statvfs.f_bsize as u64,
-                        blocks: statvfs.f_blocks as u64,
-                        bfree: statvfs.f_bfree as u64,
-                        bavail: statvfs.f_bavail as u64,
-                        inodes: Inodes::new(files, ffree, favail),
+                        bsize,
+                        blocks,
+                        bfree,
+                        bavail,
+                        inodes,
                     })
                 }
                 _ => {
+                    // println!("couldn't read {:?}", mount_point);
                     // the filesystem wasn't found, it's a strange one, for example a
-                    // docker one
+                    // docker one, or a disconnected remote one
                     None
                 }
             }
