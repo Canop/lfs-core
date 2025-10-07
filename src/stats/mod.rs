51c07a989c31635e17e6d21fd1d6acd5a90fcb2f
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2025-10-07 17:56:16 +0200

    Introduce platform specific stats implementation

---
+++ src/stats/mod.rs
@@ -0,0 +1,23 @@
+#[cfg(target_family = "unix")]
+mod unix;
+#[cfg(windows)]
+mod windows;
+
+#[cfg(target_family = "unix")]
+pub use unix::*;
+#[cfg(windows)]
+pub use windows::*;
+
+#[derive(Debug, snafu::Snafu, Clone, Copy, PartialEq, Eq)]
+#[snafu(visibility(pub(crate)))]
+pub enum StatsError {
+    #[snafu(display("Could not stat mount point"))]
+    Unreachable,
+
+    #[snafu(display("Unconsistent stats"))]
+    Unconsistent,
+
+    /// Options made us not even try
+    #[snafu(display("Excluded"))]
+    Excluded,
+}
--- src/stats/unix.rs
+++ src/stats/unix.rs
@@ -20,20 +20,6 @@ pub struct Stats {
     pub inodes: Option<Inodes>,
 }
 
-#[derive(Debug, snafu::Snafu, Clone, Copy, PartialEq, Eq)]
-#[snafu(visibility(pub(crate)))]
-pub enum StatsError {
-    #[snafu(display("Could not stat mount point"))]
-    Unreachable,
-
-    #[snafu(display("Unconsistent stats"))]
-    Unconsistent,
-
-    /// Options made us not even try
-    #[snafu(display("Excluded"))]
-    Excluded,
-}
-
 impl Stats {
     pub fn size(&self) -> u64 {
         self.bsize * self.blocks
